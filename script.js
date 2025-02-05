ec7f9e36573515bb984a774b980629af5bd040ac
peterkang34 <peter.kang@barrelny.com>
2025-02-05 15:37:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file is currently empty as there's no complex JavaScript functionality required.
+// You can add any future JavaScript enhancements or interactivity here.
+
+console.log("VeraOps site loaded successfully!")
+
